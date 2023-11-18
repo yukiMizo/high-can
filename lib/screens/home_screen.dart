4915c3b82acb5a30a584a32a3cd6214055aa377e
yukiMizo <yuki.mizo@icloud.com>
2023-11-18 23:44:45 +0900

    set up

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = "/home-screen";
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ホーム")),
+    );
+  }
+}
---
+++ lib/themes/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const themeColor = Colors.cyan;
+  static const accentColor = Colors.amber;
+  static const scaffoldBackgroundColor = Colors.white;
+  //text color
+  static const darkText1 = Colors.black;
+  static const lightText1 = Colors.white;
+}
